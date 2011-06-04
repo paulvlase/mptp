bad75f2f6be4c1eea470abbac584f3ad991f462c
Bogdan Drutu <bogdandrutu@gmail.com>
2011-06-04 11:58:25 +0300

    Add draft tests for sendmsg/recvmsg

--- src/raw/test/test_sw_recvmsg.c
+++ src/raw/test/test_sw_recvmsg.c
@@ -31,6 +31,12 @@ void recvmsg_test_suite(void)
 {
 	start_suite();
 	recvmsg_dummy();
+
+	recvmsg_invalid_descriptor();
+	recvmsg_descriptor_is_not_a_socket();
+	recvmsg_socket_is_not_bound();
+	recvmsg_after_sendto_ok();
+	recvmsg_after_sendmsg_ok();
 }
 
 static void recvmsg_dummy(void) 
@@ -38,3 +44,23 @@ static void recvmsg_dummy(void)
 	test(1 == 1);
 }
 
+static void recvmsg_invalid_descriptor(void)
+{
+	test (1 == 0);
+}
+static void recvmsg_descriptor_is_not_a_socket(void)
+{
+	test (1 == 0);
+}
+static void recvmsg_socket_is_not_bound(void)
+{
+	test (1 == 0);
+}
+static void recvmsg_after_sendto_ok(void)
+{
+	test (1 == 0);
+}
+static void recvmsg_after_sendmsg_ok(void)
+{
+	test (1 == 0);
+}
--- src/raw/test/test_sw_sendmsg.c
+++ src/raw/test/test_sw_sendmsg.c
@@ -30,9 +30,33 @@ void sendmsg_test_suite(void)
 {
 	start_suite();
 	sendmsg_dummy();
+	sendmsg_invalid_descriptor();
+	sendmsg_descriptor_is_not_socket();
+	sendmsg_socket_is_not_bound();
+	sendmsg_ok();
 }
 
 static void sendmsg_dummy(void)
 {
-	test(1 == 1);
+	test (1 == 1);
+}
+
+static void sendmsg_invalid_descriptor(void)
+{
+	test (1 == 0);
+}
+
+static void sendmsg_descriptor_is_not_socket(void)
+{
+	test (1 == 0);
+}
+
+static void sendmsg_socket_is_not_bound(void)
+{
+	test (1 == 0);
+}
+
+static void sendmsg_ok(void)
+{
+	test (1 == 0);
 }
