5ddb1215a7b43bad32876d89e57108584f3b462b
Adrian Bondrescu <adi.bondrescu@gmail.com>
2012-05-27 14:03:20 +0300

    Integrate changes on tests

--- test/client.c
+++ test/client.c
@@ -6,6 +6,21 @@
 #include <string.h>
 #include <netinet/in.h>
 #include <stdlib.h>
+#include <time.h>
+
+#define ADDR 0x8082A8C0
+#define DADDR 0x8082A8C0
+
+int gen_port()
+{
+	int ret;
+	srand(time(NULL));
+	ret = (rand() % 255) + 1;
+	if (ret == 100 || ret == 101)
+		ret *= 2;
+	printf("Generated source port %d\n", ret);
+	return ret;
+}
 
 int main(int argc, const char *argv[])
 {
@@ -22,8 +37,8 @@ int main(int argc, const char *argv[])
     memset(saddr, 0, size);
 
     saddr->count = 1;
-    saddr->dests[0].addr = 0x0100007F;
-    saddr->dests[0].port = 50;
+    saddr->dests[0].addr = ADDR;
+    saddr->dests[0].port = gen_port();
 
     if (bind(sock, (struct sockaddr *) saddr, size) < 0) {
         perror("Failed to bind socket");
@@ -31,7 +46,7 @@ int main(int argc, const char *argv[])
         return -1;
     }
 
-    char buf[] = "Buffer de test";
+    char buf[] = "Buffer1";
     char buf2[] = "Buffer2";
     struct iovec iov[2];
     struct msghdr msg;
--- test/server.c
+++ test/server.c
@@ -7,9 +7,13 @@
 #include <netinet/in.h>
 #include <stdlib.h>
 
+#define ADDR 0x8182A8C0
+#define NUM_BUF 10
+
 int main(int argc, const char *argv[])
 {
     int sock;
+	int i;
 
     if (argc != 2) {
         fprintf(stderr, "USAGE: %s listening_port\n", argv[0]);
@@ -36,24 +40,29 @@ int main(int argc, const char *argv[])
         return -1;
     }
 
-    char buf[256];
-    struct iovec iov[1];
+    char buf[NUM_BUF][10240];
+    struct iovec iov[NUM_BUF];
     struct msghdr msg;
+	size += (NUM_BUF - 1) * sizeof(struct swift_dest);
     struct sockaddr_swift *from = malloc(size);
 
     memset(&msg, 0, sizeof(msg));
     memset(&iov, 0, sizeof(iov));
     memset(from, 0, size);
 
-    iov[0].iov_base = buf;
-    iov[0].iov_len = sizeof(buf);
+	for (i = 0; i < NUM_BUF; i++) {
+		iov[i].iov_base = buf[i];
+		iov[i].iov_len = sizeof(buf[i]);
+	}
 
     msg.msg_iov = iov;
-    msg.msg_iovlen = 1;
+    msg.msg_iovlen = 10;
     msg.msg_name = from;
     msg.msg_namelen = size;
 
     int ret, fromlen;
+
+	sleep(20);
 
     ret = recvmsg(sock, &msg, 0);
     if (ret < 0) {
@@ -62,7 +71,9 @@ int main(int argc, const char *argv[])
     }
 
     printf("Received %d bytes on socket\n", ret);
-    printf("buf=%s\n", buf);
+	for (i = 0; i < from->count; i++) {
+		printf("buf=%s from %s:%d\n", buf[i], inet_ntoa(from->dests[i].addr), from->dests[i].port);
+	}
 
     if (close(sock) < 0) {
         perror("Failed to close socket");
